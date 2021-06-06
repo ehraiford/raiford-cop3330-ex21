42885f91838c6c672fcb860e0331a9417f32a3eb
Evan Raiford <84350467+ehraiford@users.noreply.github.com>
2021-06-05 23:33:07 -0400

    Implemented default as its own else statment.

--- src/main/java/ex21/App.java
+++ src/main/java/ex21/App.java
@@ -7,54 +7,57 @@ package ex21;
 import java.util.Scanner;
 
 public class App {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
         System.out.print("Please enter the number of the month: ");
         int month = in.nextInt();
         String monthName;
 
-        switch(month) {
-            case 1 :
+        switch (month) {
+            case 1:
                 monthName = "January";
                 break;
-            case 2 :
+            case 2:
                 monthName = "February";
                 break;
-            case 3 :
+            case 3:
                 monthName = "March";
                 break;
-            case 4 :
+            case 4:
                 monthName = "April";
                 break;
-            case 5 :
+            case 5:
                 monthName = "May";
                 break;
-            case 6 :
+            case 6:
                 monthName = "June";
                 break;
-            case 7 :
+            case 7:
                 monthName = "July";
                 break;
-            case 8 :
+            case 8:
                 monthName = "August";
                 break;
-            case 9 :
+            case 9:
                 monthName = "September";
                 break;
-            case 10 :
+            case 10:
                 monthName = "October";
                 break;
-            case 11 :
+            case 11:
                 monthName = "November";
                 break;
             case 12:
                 monthName = "December";
                 break;
             default:
-                monthName = "INVALID INPUT";
-        }
+                monthName = "Error. Input outside of 12 month range.";
 
-        System.out.println("The name of the month is " + monthName + ".");
+        }
+        if (month <= 12 && month >= 1) {
+            System.out.println("The name of the month is " + monthName + ".");
+        }else
+            System.out.println((monthName));
     }
 }
